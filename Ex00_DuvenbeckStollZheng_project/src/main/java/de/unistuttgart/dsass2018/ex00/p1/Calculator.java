24b383e21a3aab4c152078436f23633aea609f88
Lennart <33062821+Darkst3ve@users.noreply.github.com>
2018-04-16 18:29:43 +0200

    Add files via upload

--- Ex00_DuvenbeckStollZheng_project/src/main/java/de/unistuttgart/dsass2018/ex00/p1/Calculator.java
+++ Ex00_DuvenbeckStollZheng_project/src/main/java/de/unistuttgart/dsass2018/ex00/p1/Calculator.java
@@ -1,7 +1,53 @@
 package de.unistuttgart.dsass2018.ex00.p1;
 
+/**
+ * Class for Uebungsblatt 0 - Aufgabe 1.
+ * 
+ * @author Lennart Duvenbeck | 12345678
+ * @author Timo Stoll | 12345678
+ * @author David Zheng | 12345678
+ * 
+ */
 public class Calculator implements ICalculator {
 
+	@Override
+	public int add(int a, int b) {
+		return a + b;
+	}
 
+	@Override
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	@Override
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	@Override
+	public int max(int a, int b) {
+		if (a >= b) {
+			return a;
+		} else {
+			return b;
+		}
+	}
+
+	@Override
+	public int quersumme(int a) {
+		int result = 0;
+		while (a != 0) {
+			result += a % 10;
+			a = a / 10;
+		}
+		return result;
+	}
+
+	// public static void main(String[] args) {
+	// Calculator test = new Calculator();
+	// System.out.println(test.quersumme(516));
+	// System.out.println(test.quersumme(12345));
+	// }
 
 }
--- Ex00_DuvenbeckStollZheng_project/src/main/java/de/unistuttgart/dsass2018/ex00/p2/VariableStorage.java
+++ Ex00_DuvenbeckStollZheng_project/src/main/java/de/unistuttgart/dsass2018/ex00/p2/VariableStorage.java
@@ -1,7 +1,33 @@
 package de.unistuttgart.dsass2018.ex00.p2;
 
+/**
+ * Class for Uebungsblatt 0 - Aufgabe 2.
+ * 
+ * @author Lennart Duvenbeck | 12345678
+ * @author Timo Stoll | 12345678
+ * @author David Zheng | 12345678
+ * 
+ */
 public class VariableStorage<T> implements IVariableStorage<T> {
 
-	
+	T content = null;
+
+	@Override
+	public void set(T var) {
+		content = var;
+
+	}
+
+	@Override
+	public T get() {
+		return content;
+	}
+
+	public static void main(String[] args) {
+		VariableStorage<Integer> test = new VariableStorage<Integer>();
+		test.set(17);
+		System.out.println(test.get());
+
+	}
 
 }
