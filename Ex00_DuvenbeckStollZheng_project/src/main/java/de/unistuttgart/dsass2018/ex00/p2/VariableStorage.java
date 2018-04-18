03e11587fd79b013ea01887df97a3f51e36663c5
StollTimo <36539450+StollTimo@users.noreply.github.com>
2018-04-18 22:15:54 +0200

    Update VariableStorage.java

--- Ex00_DuvenbeckStollZheng_project/src/main/java/de/unistuttgart/dsass2018/ex00/p2/VariableStorage.java
+++ Ex00_DuvenbeckStollZheng_project/src/main/java/de/unistuttgart/dsass2018/ex00/p2/VariableStorage.java
@@ -4,7 +4,7 @@ package de.unistuttgart.dsass2018.ex00.p2;
  * Class for Uebungsblatt 0 - Aufgabe 2.
  * 
  * @author Lennart Duvenbeck | 2836913
- * @author Timo Stoll | 12345678
+ * @author Timo Stoll | 2976666
  * @author David Zheng | 3334362
  * 
  */
