1fd59f863179f5164004eddd7e5f5c97cb5aea79
Lennart <33062821+Darkst3ve@users.noreply.github.com>
2018-04-22 15:57:01 +0200

    Add files via upload

---
+++ Ex01_DuvenbeckStollZheng_project/Ex01_DuvenbeckStollZheng_project/src/main/java/de/unistuttgart/dsass2018/ex01/p2/SpeedList.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.dsass2018.ex01.p2;
+
+/**
+ * Class for Uebungsblatt 1 - Aufgabe 2.
+ * 
+ * @author Lennart Duvenbeck | 2836913
+ * @author Timo Stoll | 2976666
+ * @author David Zheng | 3334362
+ * 
+ */
+public class SpeedList<T> implements ISpeedList<T> {
+
+}
---
+++ Ex01_DuvenbeckStollZheng_project/Ex01_DuvenbeckStollZheng_project/src/main/java/de/unistuttgart/dsass2018/ex01/p3/Queue.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.dsass2018.ex01.p3;
+
+/**
+ * Class for Uebungsblatt 1 - Aufgabe 3.
+ * 
+ * @author Lennart Duvenbeck | 2836913
+ * @author Timo Stoll | 2976666
+ * @author David Zheng | 3334362
+ * 
+ */
+public class Queue<T> implements IQueue<T> {
+
+}
