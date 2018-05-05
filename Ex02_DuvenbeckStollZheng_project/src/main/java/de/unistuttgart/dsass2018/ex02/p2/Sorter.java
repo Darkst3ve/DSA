10dee3e9e300cb83afde131016063087eab31b62
Lennart <lennart.duvenbeck@gmail.com>
2018-05-05 17:47:40 +0200

    Blatt 2

---
+++ Ex02_DuvenbeckStollZheng_project/src/main/java/de/unistuttgart/dsass2018/ex02/p2/Sorter.java
@@ -0,0 +1,22 @@
+package de.unistuttgart.dsass2018.ex02.p2;
+
+/**
+ * Class for Uebungsblatt 2 - Aufgabe 2.
+ * 
+ * @author Lennart Duvenbeck | 2836913
+ * @author Timo Stoll | 2976666
+ * @author David Zheng | 3334362
+ * 
+ */
+public class Sorter {
+	
+	public static <T extends Comparable<T>> void selectionSort(ISimpleList<T> list) {
+	}
+
+	public static <T extends Comparable<T>> void bubbleSort(ISimpleList<T> list) {
+	}
+
+	public static <T extends Comparable<T>> void quickSort(ISimpleList<T> list) {
+	}
+	
+}
