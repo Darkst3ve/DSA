3e2109aba83e2fb97ca6055904871d689558de40
Lennart <lennart.duvenbeck@gmail.com>
2018-06-10 19:06:49 +0200

    Blatt 6

---
+++ Ex06_DuvenbeckStollZheng_project/src/main/java/de/unistuttgart/dsass2018/ex06/p4/Sorter.java
@@ -0,0 +1,17 @@
+package de.unistuttgart.dsass2018.ex06.p4;
+
+/**
+ * Class for Uebungsblatt 6 - Aufgabe 4.
+ * 
+ * @author Lennart Duvenbeck | 2836913
+ * @author Timo Stoll | 2976666
+ * @author David Zheng | 3334362
+ * 
+ */
+public class Sorter {
+
+	public static <T extends Comparable<T>> void heapSort(ISimpleList<T> list) {
+	}
+
+
+}
