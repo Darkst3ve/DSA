fac312b99c93e6dd2dade9568c10b868244cd5f5
Lennart <lennart.duvenbeck@gmail.com>
2018-06-25 19:59:04 +0200

    Blatt 8

---
+++ Ex08_DuvenbeckStollZheng_project/src/main/java/de/unistuttgart/dsass2018/ex08/p1/BinarySearchTree.java
@@ -0,0 +1,78 @@
+package de.unistuttgart.dsass2018.ex08.p1;
+
+import java.awt.Point;
+
+/**
+ * Class for Uebungsblatt 8 - Aufgabe 3.
+ * 
+ * @author Lennart Duvenbeck | 2836913
+ * @author Timo Stoll | 2976666
+ * @author David Zheng | 3334362
+ * 
+ */
+public class BinarySearchTree<T extends Comparable<T>> implements
+		IBinarySearchTree<T> {
+
+	private volatile IBinaryTreeNode<T> root;
+
+	public BinarySearchTree() {
+		this.root = null;
+	}
+
+	@Override
+	public void insert(T t) {
+		this.root = this.insert(this.root, t, 0, null);
+	}
+
+	private IBinaryTreeNode<T> insert(IBinaryTreeNode<T> node, T t, int lev,
+			IBinaryTreeNode<T> par) {
+		if (node == null) {
+			IBinaryTreeNode<T> newNode = new BinaryTreeNode<>();
+			newNode.setValue(t);
+			return newNode;
+		}
+
+		if (t.compareTo(node.getValue()) < 0) {
+			// Go left
+			node.setLeftChild(this.insert(node.getLeftChild(), t, lev + 1, node));
+		} else if (t.compareTo(node.getValue()) > 0) {
+			// Go right
+			node.setRightChild(this.insert(node.getRightChild(), t, lev + 1,
+					node));
+		}
+		return node;
+	}
+
+	@Override
+	public IBinaryTreeNode<T> getRootNode() {
+		return this.root;
+	}
+
+	@Override
+	public boolean isFull() {
+		return this.isFull(this.root);
+	}
+
+	private boolean isFull(IBinaryTreeNode<T> node) {
+		if (node == null) {
+			return true;
+		} else if ((node.getLeftChild() == null)
+				&& (node.getRightChild() == null)) {
+			return true;
+		} else if ((node.getLeftChild() == null)
+				|| (node.getRightChild() == null)) {
+			return false;
+		}
+		return this.isFull(node.getLeftChild())
+				&& this.isFull(node.getRightChild());
+	}
+
+	@Override
+	public void calculatePositions() {
+		// TODO calculate the position for each node in the tree
+		// - the position of a node on the x-axis is calculated by its position in the in-order traversal (first node has x = 0)
+		// - the position of a node on the y-axis is calculated by its level in the tree (root has y = 0)
+	}
+
+
+}
---
+++ Ex08_DuvenbeckStollZheng_project/src/main/java/de/unistuttgart/dsass2018/ex08/p1/IBinarySearchTree.java
@@ -0,0 +1,34 @@
+package de.unistuttgart.dsass2018.ex08.p1;
+
+public interface IBinarySearchTree<T extends Comparable<T>> {
+
+	/**
+	 * Inserts a node with the given element into the binary search tree. If the
+	 * tree already contains this element, discard the insertion.
+	 * 
+	 * @param t
+	 *            the element to be inserted
+	 */
+	public void insert(T t);
+
+	/**
+	 * Returns the (true) root node of the binary search tree.
+	 * 
+	 * @return root node of the tree
+	 */
+	public IBinaryTreeNode<T> getRootNode();
+
+	/**
+	 * Returns a boolean whether the tree is full. See slides V05 page 20 for
+	 * the definition of a full tree. Here, "full" refers to the German "voll".
+	 * 
+	 * @return a boolean whether the tree is full
+	 */
+	public boolean isFull();
+
+	/**
+	 * calculates the positions of the nodes for drawing
+	 */
+	public void calculatePositions();
+
+}
